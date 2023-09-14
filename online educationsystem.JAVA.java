971e596ac69211eeeb9b8fcaaefb5cbc04551cd9
MamathaS-hub <144894478+MamathaS-hub@users.noreply.github.com>
2023-09-14 22:03:52 +0530

    Add files via upload

---
+++ online educationsystem.JAVA.java
@@ -0,0 +1,59 @@
+DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-traction .d; 
+<head>
+<meta http- per ="Content-Type" content="text/html; charset=UTF-8" /> <link 
+href="bootstrap/css/bootstrap.min.css" real ="stylesheet">
+<link href="bootstrap/css/ - " l"> <link type="text/css" DOCTYPE PUBLIC "-//W3C//DTD 
+XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional ">
+<head>
+<meta http- ="Content-Type" content="text/html; charset=UTF-8" /> <link
+href="bootstrap/css/bootstrap.min.css" re="stylesheet">
+sink ="stylesheet"> <link type="text/css" re="stylesheet" href="css/style.css" />
+scrips
+<title>Bus Scheduler</title> </head>
+<body>
+<div class="container">
+<div class="header">
+<a class="pull-left" href="index. "><b>Bus Scheduler</b></a> <p class="pull-right">
+<a href=" ">Sign Up</a> / <a href="sign . ">Sign In</a> </p>
+</div> <%
+String msg=(String)session.getAttribute("msg"); if(msg!=null)
+{
+%>
+<br>
+<div class="panel panel-danger">
+<div class="panel-heading text- "> <p><%=msg%></p>
+</div> </div>
+<%
+session.setAttribute("msg", null); }
+%>
+<div class="row"> <br/>
+<div class="pull-left">
+< src=" /signup.jpg">
+</div>
+<div class=" pull-right">
+<h5 class="text- "> Fill up the form to Sign Up </h5>="stylesheet" href="css/style.css" />
+<script type="text/javascript" src="1.8.2.m"></script> <script src="bootstrap/."></script>
+<title>Bus Scheduler</title> </head
+<body>
+<div class="container">
+<div class="header">
+<a class="pull-left" href="index."><b>Bus Scheduler</b></a> <p class="pull-right">
+<a href=" Up</a> / <a href=" ">Sign In</a> </p>
+</div> <%
+String msg=(String)session.getAttribute("msg"); if(msg!=null)
+{
+%>
+<br>
+<div class="panel panel-danger">
+<div class="panel-heading text- "> <p><%=msg%></p>
+</div> </div>
+<%
+session.setAttribute("msg", null); }
+%>
+<div class="row"> <br/>
+<div class="pull-left">
+<m src=" /signup.jpg">
+</div>
+<div class= col-ad-4 pull-right">
+<h5 class=Fill up the form to Sign Up </h5>
